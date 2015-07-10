c1fe73c2c69d46faa0ef17a8a32d51a25877e5b0
Timothy Yong <tyong_23@hotmail.com>
2015-07-10 03:57:30 -0400

    removed old

--- robot/construct/construct.cpp
+++
@@ -1,9 +0,0 @@
-#include "construct.h"
-
-Link::Link(
-
-Joint::Joint(double min, double max, double initial_value) {
-  this->min = min;
-  this->max = max;
-  this->value = min; // usually the starting point is the min value
-}
