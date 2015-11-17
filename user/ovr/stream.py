229955a3bbb0bdafab3e4458ef3abed5b0ed2bf6
Timothy Yong <tyong_23@hotmail.com>
2015-11-17 01:20:23 -0500

    deleting dups

--- user/ovr/stream.py
+++
@@ -1,8 +0,0 @@
-from cv2 import *
-
-def main():
-  v = VideoWriter("stream.avi", -1, 20, (640, 480))
-  if not v.isOpened():
-    print "Cannot open"
-
-main()
--- user/rtsp/gstscripts/get_basic.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gst-launch-1.0 tcpclientsrc port=8554 host=localhost  ! tsdemux ! h264parse ! avdec_h264 ! xvimagesink 
